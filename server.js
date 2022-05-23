a0dc22aa8544020314ed2a81285d6cdd8eb25c4c
aabdulsalams <abdulsalamsabil@gmail.com>
2022-05-23 11:56:36 +0700

    add create roles

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const db = require('./app/models');
 
 const app = express();
-const corsOptions = {
-  origin: 'http://localhost:8081',
-};
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-  res.json({ message: 'Jamin Padi application' });
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(cookieParser());
+app.use(express.static('app/public'));
+
+// Set app config
+const title = process.env.TITLE;
+const port = process.env.PORT;
+const baseUrl = process.env.URL + port;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+require('./app/router/router.js')(app);
+
+db.sequelize.sync({ force: true }).then(() => {
+  create_roles();
+  app.listen(port, () => console.log(`${title} run on ${baseUrl}`));
 });
+
+function create_roles() {
+  db.Role.create({
+    id: 1,
+    name: 'USER',
+  });
+
+  db.Role.create({
+    id: 2,
+    name: 'SUPERUSER',
+  });
+}
