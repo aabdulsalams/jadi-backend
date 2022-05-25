14552d865012a6b43ce99dde214b8aa2e923ecf9
aabdulsalams <abdulsalamsabil@gmail.com>
2022-05-26 06:14:54 +0700

    add histories table migration

---
+++ app/migrations/20220524093803-create-history.js
@@ -0,0 +1,23 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Histories', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+      },
+      disease_id: {
+        type: Sequelize.STRING,
+      },
+      scan_date: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Histories');
+  },
+};
