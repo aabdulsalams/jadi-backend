11072c875fa0785f3f1361ca66cb3c6d6e33e2b3
aabdulsalams <abdulsalamsabil@gmail.com>
2022-05-30 12:50:21 +0700

    fix bearer token check

--- app/api/verifyJwtToken.js
+++ app/api/verifyJwtToken.js
@@ -4,7 +4,17 @@ const { User } = require('../models');
 
 module.exports = {
   verifyToken(req, res, next) {
-    const token = req.headers['x-access-token'];
+    const bearerHeader = req.headers.authorization;
+
+    if (bearerHeader.split(' ')[0] !== 'Bearer') {
+      return res.status(500).send({
+        auth: false,
+        message: 'Error',
+        errors: 'Incorrect token format',
+      });
+    }
+    const bearerToken = bearerHeader.split(' ');
+    const token = bearerToken[1];
     if (!token) {
       return res.status(403).send({
         auth: false,
