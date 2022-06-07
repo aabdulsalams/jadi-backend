e5481ea3b3d821844ef1b8e606eb1f5396a41a01
aabdulsalams <abdulsalamsabil@gmail.com>
2022-06-07 16:25:45 +0700

    add description

--- app/migrations/20220524133459-create-disease.js
+++ app/migrations/20220524133459-create-disease.js
@@ -9,6 +9,9 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
       },
+      description: {
+        type: Sequelize.STRING,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
